e4afdfef44d4f795e1f41f135cacda572bc892e2
PrashantDhungana <sharma.prashant2000@gmail.com>
2023-01-04 11:58:36 +0545

    [ADD] Commit

---
+++ DesktopModel/Services/Auth.cs
@@ -0,0 +1,33 @@
+﻿using DesktopModel.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopModel.Services
+{
+    public class Auth : IAuth
+    {
+        public async Task<UserDetails> AuthenticateUsers(LoginModel loginModel)
+        {
+            var returnResponse = new UserDetails();
+			var stream = await FileSystem.OpenAppPackageFileAsync("wwwroot/Userauth.json");
+			var reader = new StreamReader(stream);
+            var UserAuth = await reader.ReadToEndAsync();
+            var userBasicDetail = JsonConvert.DeserializeObject<List<UserAuthDetails>>(UserAuth);
+            foreach (var item in userBasicDetail)
+            {
+                if (item.Email == loginModel.UserName && item.Password == loginModel.Password)
+                {
+                    returnResponse.Name = item.Name;
+                    returnResponse.Email = item.Email;
+                    returnResponse.RoleId = item.RoleId;
+                    break;
+                }
+            }
+            return returnResponse;
+        }
+    }
+}
---
+++ DesktopModel/Services/IAuth.cs
@@ -0,0 +1,14 @@
+﻿using DesktopModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopModel.Services
+{
+    public interface IAuth
+    {
+        public Task<UserDetails> AuthenticateUsers(LoginModel loginModel);
+    }
+}
